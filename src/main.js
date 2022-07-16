574e594074900c27c0605afbbbe0cf9af42c53ad
JesusMNA <jesuso_0709@hotmail.com>
2022-07-16 00:15:23 -0500

    Se arreglo el problema de la carga duplicada de datos

--- src/main.js
+++ src/main.js
@@ -12,8 +12,8 @@ async function getTrendigMoviesPreview() {
     const { data } = await api('trending/movie/day');
 
     const movies = data.results;
+    trendingMoviesPreviewList.innerHTML = "";
     movies.forEach(movie => {
-        const trendingMoviesPreviewList = document.querySelector('#trendingPreview .trendingPreview-movieList')
 
         const movieContainer = document.createElement('div');
         movieContainer.classList.add('movie-container');
@@ -35,8 +35,8 @@ async function getCategoriesPreview() {
     const { data } = await api('genre/movie/list');
 
     const categories = data.genres;
+    categoriesPreviewList.innerHTML = "";
     categories.forEach(category => {
-        const categoriesPreviewList = document.querySelector('#categoriesPreview .categoriesPreview-list')
 
         const categoryContainer = document.createElement('div');
         categoryContainer.classList.add('category-container');
